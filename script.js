688a2b37a13ba9918902741be4c63bbb6b55c57d
Guilherme gg <ggdev0001@gmail.com>
2026-08-12 15:43:15 -0300

    tirei o scroll reveal e voltei pro que tava funcionando

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import { initSplashCursor } from "./splash-cursor.js";
-import { initScrollReveal } from "./scroll-reveal.js";
 
 const root = document.documentElement;
 const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
@@ -56,17 +55,6 @@ function mountSplashCursor() {
 }
 
 mountSplashCursor();
-
-const cleanupScrollReveal = initScrollReveal({
-  baseOpacity: 0.1,
-  enableBlur: true,
-  baseRotation: 3,
-  blurStrength: 4,
-});
-
-window.addEventListener("pagehide", (event) => {
-  if (!event.persisted) cleanupScrollReveal();
-}, { once: true });
 
 const intro = document.querySelector(".intro-screen");
 if (intro) {
